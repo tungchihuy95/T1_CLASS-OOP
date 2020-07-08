cd2015cfa04746753816d20bbf8e99c7abb7a543
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-08 11:52:30 +0700

    jklajg

--- src/RectangleClass/Rectangle.java
+++ src/RectangleClass/Rectangle.java
@@ -1,4 +1,29 @@
 package RectangleClass;
 
-public class Rectangel {
+public class Rectangle {
+       //Bước 1: Tạo lớp Rectangle, khai báo các thuộc tính (properties), định nghĩa các phương thức khởi tạo (contructors).
+           double width, height;
+
+           public Rectangle() {
+           }
+
+           public Rectangle(double width, double height) {
+               this.width = width;
+               this.height = height;
+           }
+           //code below here
+    public double getArea() {
+               return this.height * this.width;
+    }
+    public double getPerimeter() {
+               return (this.height + this.width) * 2;
+    }
+    public String display() {
+               return "Rectangle {" + "width =" + this.width + ", height =" + this.height + "}";
+
+    }
 }
+
+
+
+
