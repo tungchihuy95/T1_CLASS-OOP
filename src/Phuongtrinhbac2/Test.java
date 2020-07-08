265843245d2a95e410755b5b15d83a2a2c77decb
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-08 21:37:34 +0700

    move

--- src/Phuongtrinhbac2/Test.java
+++ src/Phuongtrinhbac2/Test.java
@@ -1,4 +1,29 @@
 package Phuongtrinhbac2;
 
+import RectangleClass.Rectangle;
+
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        double a = scanner.nextDouble();
+        System.out.println("Nhap b:");
+        double b = scanner.nextDouble();
+        System.out.println("Nhap c:");
+        double c = scanner.nextDouble();
+        //System.out.println("Delta la:");
+        //double delta = scanner.nextDouble();
+
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+        if (quadraticEquation.getDiscriminant() > 0) {
+            System.out.println("Nghiem 1 cua phuong trinh la:" + quadraticEquation.getRoot1());
+            System.out.println("Nghiem 2 cua phuong trinh la:" + quadraticEquation.getRoot2());
+        } else if (quadraticEquation.getDiscriminant() == 0) {
+            System.out.println("Phuong trinh co 1 nghiem la:" + quadraticEquation.getRoot3());
+        } else {
+            System.out.println("Phuong trinh vo nghiem");
+        }
+    }
 }
